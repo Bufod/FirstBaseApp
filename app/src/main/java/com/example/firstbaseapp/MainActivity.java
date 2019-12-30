0cf9f8fe0aec177438bbdd0ddba0a82721df94da
Bufod <nikolaybonar97@gmail.com>
2019-12-30 09:40:56 +0300

    commit

--- app/src/main/java/com/example/firstbaseapp/MainActivity.java
+++ app/src/main/java/com/example/firstbaseapp/MainActivity.java
@@ -22,53 +22,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         constraintLayout = (ConstraintLayout) findViewById(R.id.CLayout);
         tw = (TextView) findViewById(R.id.textW);
-        btText = (Button) findViewById(R.id.buttonText);
-        btText.setOnClickListener(btTextClick());
-        btColorBack = (Button) findViewById(R.id.buttonBack);
-        btColorBack.setOnClickListener(btColorBackClick());
+//        btText = (Button) findViewById(R.id.buttonText);
+//        btText.setOnClickListener(btTextClick());
+//        btColorBack = (Button) findViewById(R.id.buttonBack);
+//        btColorBack.setOnClickListener(btColorBackClick());
     }
 
-    RandomString getRandomString(){
-        int n = (int) (Math.random()*randomStrings.length);
-        return randomStrings[n];
-    }
+    // получить случайную строку
 
-    int getRandomColor(){
-        int a = (int) (Math.random()*256);
-        int r = (int) (Math.random()*256);
-        int g = (int) (Math.random()*256);
-        int b = (int) (Math.random()*256);
-        return Color.argb(a,r,g,b);
-    }
 
-    View.OnClickListener btTextClick(){
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                RandomString rs = getRandomString();
-                tw.setText(rs.getText());
-                tw.setTextSize(rs.getSize());
-                tw.setMaxWidth(800);
-                btColorBack.setEnabled(true);
-                btText.setEnabled(false);
-            }
-        };
-    }
+    // получить случайный цвет
 
-    View.OnClickListener btColorBackClick(){
-        return new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                tw.setTextColor(getRandomColor());
-                constraintLayout.setBackgroundColor(getRandomColor());
-                clickCount++;
-                if (clickCount == 5){
-                    btText.setEnabled(true);
-                    clickCount = 0;
-                }
-            }
-        };
-    }
 
+    // кнопка генерации текста
+
+
+    // кнопка генерации цвета
 
 }
